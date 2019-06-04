9e601009067b4a4ed6c54e62a1ec14a3066b372b
Edmund Ng <nomaded@gmail.com>
2019-06-04 12:52:37 -0400

    Changing TAPPING_TERM to be shorter. Having timing problems with the LT keys.

--- keyboards/atreus62/keymaps/ngedv/config.h
+++ keyboards/atreus62/keymaps/ngedv/config.h
@@ -6,6 +6,11 @@
 // overrides go here!
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 3
+#undef ONESHOT_TAP_TOGGLE
+#define ONESHOT_TAP_TOGGLE 3
 #undef IS_COMMAND
 #define IS_COMMAND() (get_mods() == (MOD_BIT(KC_LSFT) | MOD_BIT(KC_LALT)))
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
 
