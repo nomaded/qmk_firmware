d681cfa1a10558da8662176d191fce206203b143
Edmund Ng <nomaded@gmail.com>
2019-06-06 13:30:15 -0400

    Adjusting the TAPPING_TERM value. 150 was too long because the layer switch was not happening fast enough. But 100 was too short because I had problems triggering ESC on the Ctrl/esc key. Trying 120 now.

--- keyboards/atreus62/keymaps/ngedv/config.h
+++ keyboards/atreus62/keymaps/ngedv/config.h
@@ -11,6 +11,6 @@
 #undef IS_COMMAND
 #define IS_COMMAND() (get_mods() == (MOD_BIT(KC_LSFT) | MOD_BIT(KC_LALT)))
 #undef TAPPING_TERM
-#define TAPPING_TERM 150
+#define TAPPING_TERM 120
 #define PERMISSIVE_HOLD
 
