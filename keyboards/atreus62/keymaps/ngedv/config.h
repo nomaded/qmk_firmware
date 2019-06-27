b132efe755472b164948daa0ab749f414eba4eb6
Edmund Ng <nomaded@gmail.com>
2019-06-27 18:00:22 -0400

    Files for new layout, with shift-to-capslock working.

---
+++ keyboards/atreus62/keymaps/ngedv/config.h
@@ -0,0 +1,16 @@
+// If you wish to override any setting set by a previous config.h
+// you will need to first include some boilerplate code for the
+// settings you wish to change.
+#pragma once
+
+// overrides go here!
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 3
+#undef ONESHOT_TAP_TOGGLE
+#define ONESHOT_TAP_TOGGLE 3
+#undef IS_COMMAND
+#define IS_COMMAND() (get_mods() == (MOD_BIT(KC_LSFT) | MOD_BIT(KC_LALT)))
+#undef TAPPING_TERM
+#define TAPPING_TERM 120
+#define PERMISSIVE_HOLD
+
