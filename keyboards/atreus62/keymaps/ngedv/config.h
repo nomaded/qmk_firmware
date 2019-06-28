176e85357a156dbf92b621fa93e78ee5077433f2
Edmund Ng <nomaded@gmail.com>
2019-06-28 16:13:50 -0400

    Added copyright and changed indents to 4 spaces (soft tab).

--- keyboards/atreus62/keymaps/ngedv/config.h
+++ keyboards/atreus62/keymaps/ngedv/config.h
@@ -1,9 +1,26 @@
-// If you wish to override any setting set by a previous config.h
-// you will need to first include some boilerplate code for the
-// settings you wish to change.
+/* Copyright 2019 Edmund C. Ng
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* If you wish to override any setting set by a previous config.h
+ * you will need to first include some boilerplate code for the
+ * settings you wish to change.
+ */
 #pragma once
 
-// overrides go here!
+/* overrides go here! */
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 3
 #undef ONESHOT_TAP_TOGGLE
