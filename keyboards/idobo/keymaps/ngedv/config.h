03f5a3a29e21928146952e9db8b7368235a82c3c
Edmund Ng <nomaded@gmail.com>
2019-06-30 20:21:19 -0400

    Working ngedv layout that compiles and loads.
    Initial 5-layer layout that matches my usual ngedv setup. The middle 3
    columns are mostly unused, still, and I have not translated my
    one-handed layers, yet. Added some keys to control the builtin RGB LEDs.

--- keyboards/idobo/keymaps/ngedv/config.h
+++ keyboards/idobo/keymaps/ngedv/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2018 MechMerlin
+/* Copyright 2019 Edmund C. Ng
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* If you wish to override any setting set by a previous config.h
+ * you will need to first include some boilerplate code for the
+ * settings you wish to change.
+ */
 #pragma once
 
-// place overrides here
+/* overrides go here! */
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 3
+#undef ONESHOT_TAP_TOGGLE
+#define ONESHOT_TAP_TOGGLE 3
+#undef IS_COMMAND
+#define IS_COMMAND() (get_mods() == (MOD_BIT(KC_LSFT) | MOD_BIT(KC_LALT)))
+#undef TAPPING_TERM
+#define TAPPING_TERM 120
+//#define PERMISSIVE_HOLD
+
